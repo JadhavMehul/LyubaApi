5b483442dfca549755fc231a00873accc9ba5f19
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-11-07 23:02:43 +0530

    get other people profile data but not well optimized

--- controllers/userDetailsController.js
+++ controllers/userDetailsController.js
@@ -65,3 +65,66 @@ exports.profileProfileData = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+
+exports.addUserInLoop = async (req, res) => {
+    // const users =  // Expecting JSON array please add data in array of object as shown below
+
+    // [
+    //     {
+    //         "id": "8l5kvaeQCnAG8c4vROYtV7nfaeAy",
+    //         "lastName": "Lee",
+    //         "pincode": "E1 6AN",
+    //         "birthdate": "14/03/1983",
+    //         "gender": "male",
+    //         "city": "London",
+    //         "personalData": {
+    //             "profession": "Lawyer",
+    //             "feet": "4 Feet",
+    //             "drinking": "No",
+    //             "education": "Primary School",
+    //             "genderPreference": "Both",
+    //             "sign": "Sagittarius",
+    //             "workingAt": "",
+    //             "religion": "Judaism",
+    //             "workout": "Active",
+    //             "smoking": "Yes",
+    //             "looking": "Marriage",
+    //             "inch": "0 Inch",
+    //             "kids": "No",
+    //             "status": "Unmarried"
+    //         },
+    //         "pictures": [
+    //             "https://firebasestorage.googleapis.com/v0/b/lyuba-dating-app.firebasestorage.app/o/users%2Fk1Tn5aUks8WWTq5m8aYc4thWI8z2%2Fphotos%2Fphoto_0.jpg?alt=media&token=f0105f13-7b54-4eac-96e7-c237d5542984",
+    //             "https://firebasestorage.googleapis.com/v0/b/lyuba-dating-app.firebasestorage.app/o/users%2FHx5bjizIdTRKFssDl0SJSP12RnR2%2Fphotos%2Fphoto_0.jpg?alt=media&token=2fad6650-ea59-4771-83c5-516a279e1528"
+    //         ],
+    //         "createdAt": "October 22, 2025 at 11:03:55 AM UTC+5:30",
+    //         "firstName": "Vikram",
+    //         "uid": "8l5kvaeQCnAG8c4vROYtV7nfaeAy",
+    //         "provider": "facebook.com",
+    //         "interests": [
+    //             "Movie",
+    //             "Football",
+    //             "Stock Market",
+    //             "Cricket"
+    //         ],
+    //         "email": "vikram.lee839@example.com"
+    //     },
+    // ]
+
+    if (!Array.isArray(users)) {
+        return res.status(400).send({ error: "Request body must be an array of users" });
+    }
+
+    try {
+        for (const user of users) {
+            await firestore.collection("users").doc(user.uid).set(user);
+            
+        }
+        res.status(200).send({ message: "All users uploaded successfully!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Failed to upload users", details: error.message });
+    }
+
+}
--- routes/userDetails.js
+++ routes/userDetails.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { profileData, profileProfileData } = require("../controllers/userDetailsController");
+const { profileData, profileProfileData, addUserInLoop } = require("../controllers/userDetailsController");
 
 router.post("/profile", profileData)
 router.post("/peopleProfile", profileProfileData)
+router.post("/addUserLoop", addUserInLoop);
 
 module.exports = router;
